53b5dddd44ea31f9986862ffafd2c166f62cf4fd
Sergey <2805026@gmail.com>
2022-12-07 22:45:03 +0300

    Home work 4 Tasks 3 added

---
+++ HWTask4/src/Task3.java
@@ -0,0 +1,34 @@
+//1) Написать метод, который принимает массив элементов, помещает их в стэк и выводит на консоль содержимое стэка.
+// 2) Написать метод, который принимает массив элементов, помещает их в очередь и выводит на консоль содержимое очереди
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class Task3 {
+    public static void main(String[] args) {
+        String[] array = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"};
+        printStack(array);
+        printQueue(array);
+    }
+
+    public static void printStack(String[] array) {
+        Stack<String> stack = new Stack<>();
+        for (String s : array) {
+            stack.push(s);
+        }
+        while (!stack.empty()) {
+            System.out.println(stack.pop());
+        }
+    }
+
+    public static void printQueue(String[] array) {
+        Queue<String> queue = new LinkedList<>();
+        for (String s : array) {
+            queue.add(s);
+        }
+        while (!queue.isEmpty()) {
+            System.out.println(queue.remove());
+        }
+    }
+
+}
